083332d2da0b3a6f706fc0c3675317efd7c0dcd6
matacarlitos2006-wq <matacarlitos2006@gmail.com>
2025-11-11 20:48:27 -0700

    Update app.js

--- Java/app.js
+++ Java/app.js
@@ -1,7 +1,5 @@
 document.getElementById("myForm").addEventListener("submit", function(event) {
   event.preventDefault(); 
-  
   alert("Your text has been submitted");
-  
   location.reload();
-}
+});
