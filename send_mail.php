36f89bb68e8b727ae054218b1a46705331e59f8c
matacarlitos2006-wq <matacarlitos2006@gmail.com>
2025-11-18 19:10:18 -0700

    Create send_mail.php

---
+++ send_mail.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name    = htmlspecialchars($_POST['name']);
+    $email   = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "matacarlitos2006@gmail.com";
+    $subject = "New Contact Form Submission";
+
+    $body = "Name: $name\nEmail: $email\nMessage:\n$message";
+
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message Sent!";
+    } else {
+        echo "Error sending message.";
+    }
+}
+?>
