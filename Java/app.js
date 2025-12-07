1fb83d5f7f5c59f6f236e27d1d0c298f51367465
matacarlitos2006-wq <matacarlitos2006@gmail.com>
2025-12-06 18:27:10 -0700

    Update links in searchData to relative paths

--- Java/app.js
+++ Java/app.js
@@ -3,32 +3,32 @@ const searchData = [
   {
     title: "Events",
     keywords: ["event", "events", "celebration", "Trunk", "Treat", "Red Rock Balloon", "Indian Ceremonial"],
-    link: "https://matacarlitos2006-wq.github.io/Gallup.github.io/cat/Event.html"
+    link: "../cat/Events.html"
   },
   {
     title: "History of Gallup",
     keywords: ["history", "gallup history", "Ancestral Puebloans", "Trading Posts", "Inter-Tribal Indian Ceremonial"],
-    link: "https://matacarlitos2006-wq.github.io/Gallup.github.io/cat/History.html"
+    link: "../cat/History.html"
   },
   {
     title: "Public Library",
     keywords: ["library", "books", "Zollinger library", "Computer Lab", "Conference Room", "Study Rooms"],
-    link: "https://matacarlitos2006-wq.github.io/Gallup.github.io/Services/Library.html"
+    link: "../cat/Library.html"
   },
   {
     title: "Educational Services",
     keywords: ["education", "school", "child care", "GMCS School", "High School", "Middle School", "Elementry School"],
-    link: "https://matacarlitos2006-wq.github.io/Gallup.github.io/Services/Education.html"
+    link: "../Services/Education.html"
   },
   {
     title: "Transportation",
     keywords: ["transport", "bus", "travel", "airplane", "Gallup Express", "Navajo Transit", "GreyHound"],
-    link: "https://matacarlitos2006-wq.github.io/Gallup.github.io/Services/Transportation.html"
+    link: "../Services/Transportation.html"
   },
   {
     title: "Contact Us",
     keywords: ["contact", "email", "phone", "address"],
-    link: "https://matacarlitos2006-wq.github.io/Gallup.github.io/#contact"
+    link: "../#contact"
   },
   {
     title: "About Us",
@@ -43,12 +43,12 @@ const searchData = [
   {
     title: "Reference",
     keywords: ["reference", "Reference", "Cite"],
-    link: "https://matacarlitos2006-wq.github.io/Gallup.github.io/Ref/Reference.html"
+    link: "../Ref/Reference.html"
   },
   {
     title: "Get Help",
     keywords: ["get help", "Police", "Help", "Lines", "help", "Urgent"],
-    link: "https://matacarlitos2006-wq.github.io/Gallup.github.io/Services/Get-Help.html"
+    link: "../Services/Get-Help.html"
   }
 ];
 
