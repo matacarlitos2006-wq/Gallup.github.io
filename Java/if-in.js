9bd6aaa21c12517a90f66b06dea41820e11afade
matacarlitos2006-wq <matacarlitos2006@gmail.com>
2026-03-30 21:32:13 -0600

    Update if-in.js

--- Java/if-in.js
+++ Java/if-in.js
@@ -83,14 +83,13 @@ function handleSearch() {
     alert(`No results found for "${query}".`);
   }
 
-  function toggleNav(){
+function toggleNav(){
 document.getElementById("sidebar").classList.toggle("active");
 }
 
 function toggleFolder(e,el){
 e.preventDefault();
 el.parentElement.classList.toggle("open");
-}
 }
 
 const cards = document.querySelectorAll('.card-home');
