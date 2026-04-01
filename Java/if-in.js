3cd28a79655a15db14a93cc56f88788e6ee8c11f
matacarlitos2006-wq <matacarlitos2006@gmail.com>
2026-03-31 20:16:08 -0600

    Update if-in.js

--- Java/if-in.js
+++ Java/if-in.js
@@ -83,34 +83,3 @@ function handleSearch() {
     alert(`No results found for "${query}".`);
   }
 
-function toggleNav(){
-document.getElementById("sidebar-ed").classList.toggle("active");
-}
-
-function toggleFolder(e,el){
-e.preventDefault();
-el.parentElement.classList.toggle("open");
-}
-
-const cards = document.querySelectorAll('.card-home');
-
-cards.forEach(card => {
-  card.addEventListener('mousemove', (e) => {
-    const rect = card.getBoundingClientRect();
-
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-
-    const centerX = rect.width / 2;
-    const centerY = rect.height / 2;
-
-    const rotateX = ((y - centerY) / centerY) * -25;
-    const rotateY = ((x - centerX) / centerX) * 25;
-
-    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-  });
-
-  card.addEventListener('mouseleave', () => {
-    card.style.transform = 'rotateX(0deg) rotateY(0deg)';
-  });
-});
