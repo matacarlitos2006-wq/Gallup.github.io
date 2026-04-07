d2e4ef2522679252c63a34f8a0aabf08b5c6dc6c
matacarlitos2006-wq <matacarlitos2006@gmail.com>
2026-04-06 20:18:37 -0600

    Update app.js

--- Java/app.js
+++ Java/app.js
@@ -148,19 +148,7 @@ e.preventDefault();
 el.parentElement.classList.toggle("open");
 }
 
-const elements = document.querySelectorAll('.fade-in-right');
-
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('show');
-        }
-      });
-    });
-
-elements.forEach(el => observer.observe(el));
-
-const elements = document.querySelectorAll('.fade-up');
+const elements = document.querySelectorAll('.fade-in-right, .fade-up');
 
     const observer = new IntersectionObserver((entries) => {
       entries.forEach(entry => {
